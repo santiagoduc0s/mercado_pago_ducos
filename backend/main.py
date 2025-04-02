c286c30ce7fe1f86ccd38ef47ab7742851e3a7b5
santiagoduc0s <santidu200@gmail.com>
2025-04-02 02:01:45 +0200

    feat(cors): remove cors problem web

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.preferences import preferences_bp
 from routes.subscriptions import subscriptions_bp
 from routes.events import events_bp
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(preferences_bp)
 app.register_blueprint(subscriptions_bp)
